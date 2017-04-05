27ad423d8d134759b7ed61efeb88f5c32e0a7c1a
Wlodek Bzyl <matwb@univ.gda.pl>
2017-04-05 11:35:14 +0200

    6.1 Generating a Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213084310) do
+ActiveRecord::Schema.define(version: 20170405093146) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "gist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gist_id"], name: "index_comments_on_gist_id"
+  end
 
   create_table "gists", force: :cascade do |t|
     t.text     "src"
