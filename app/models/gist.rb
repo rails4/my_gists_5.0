72fadba31f57f4b97e27dc7dfdf5d74c1f6fa558
Wlodek Bzyl <matwb@univ.gda.pl>
2017-04-07 10:41:01 +0200

    7.1 Rendering Partial Collections

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,4 +1,4 @@
 class Gist < ApplicationRecord
   has_many :comments
-  validates :src, presence: true, length: { minimum: 8, maximum: 256 }
+  validates :src, presence: true, length: { minimum: 8, maximum: 1024 }
 end
